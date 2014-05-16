d577b6338ef35f8bd8a434a3351feb5674bc53c9
gubi <gubi.ale@iod.io>
2014-05-16 17:18:56 +0200

    Trait search is working

--- common/js/_main/form.js
+++ common/js/_main/form.js
@@ -42,11 +42,47 @@ $.close_chosen = function() { $(this).trigger("chosen:close"); };
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 /**
+/* Add collapsible content
+*/
+$.fn.addCollapsible = function(options, callback) {
+	var options = $.extend({
+		title: "Collapsible panel",
+		icon: "fa fa-search",
+		content: "",
+		id: $.makeid()
+	}, options);
+	
+	if($("#accordion").length == 0) {
+		$(this).append('<div class="panel-group" id="accordion">');
+	} else {
+		$(".collapse").collapse("hide");
+	}
+	
+	var root_node = $('<div class="panel panel-default">'),
+		node_heading = $('<div class="panel-heading">'),
+			node_heading_title = $('<h4 class="panel-title"><span class="' + options.icon + '"></span>&nbsp;&nbsp;<a data-toggle="collapse" data-parent="#accordion" href="#' + options.id + '">' + options.title + '</a></h4>'),
+		node_body_collapse = $('<div id="' + options.id + '" class="panel-collapse collapse in">'),
+			node_body = $('<div class="panel-body">' + options.content + '</div>');
+	
+	
+	node_heading_title.appendTo(node_heading);
+	node_heading.appendTo(root_node);
+	node_body.appendTo(node_body_collapse);
+	node_body_collapse.appendTo(root_node);
+	
+	$(this).find("#accordion").append(root_node);
+
+	
+	if (typeof callback == "function") {
+		callback.call(this);
+	}
+};
+
+/**
 /* Add the main trait form
 */
 $.fn.addTraitAutocomplete = function(options, data, callback) {
 	// GET REQUEST URL: http://pgrdg.grinfo.private/Service.php?op=ping
-	
 	var options = $.extend({
 		id: "",
 		class: "",
@@ -54,8 +90,10 @@ $.fn.addTraitAutocomplete = function(options, data, callback) {
 	}, options);
 	var op_btn_list = "",
 	selected_label = "Operator",
-	radio = "",
-	user_input;
+	checkbox = "",
+	user_input,
+	selected_label_key = "",
+	selected_label_value = "";
 	
 	$.selected_menu = function(k, v) {
 		var kk = k.replace("$", "");
@@ -65,21 +103,27 @@ $.fn.addTraitAutocomplete = function(options, data, callback) {
 		$("#" + options.id).focus();
 	};
 	$.each(options.op, function(k, v) {
-		if(v.key == "$i") {
-			radio += '<div class="checkbox"><label><input type="checkbox" id="' + options.id + '_operator_i" ' + ((v.selected) ? 'checked="checked"' : "") + ' value="" /> ' + v.label + '</label></div>';
+		if(!v.main) {
+			if(v.label !== undefined) {
+				checkbox += '<div class="checkbox"><label title="' + ((v.title !== undefined) ? v.title : "") + '"><input type="checkbox" id="' + options.id + '_operator_' + v.key.replace("$", "") + '" ' + ((v.selected) ? 'checked="checked"' : "") + ' title="' + ((v.title !== undefined) ? v.title : "") + '" value="" /> ' + v.label + '</label></div>';
+			}
 		} else {
-			if(v.title == undefined) {
-				if(v.selected){
-					selected_label_key = v.key;
-					selected_label_value = v.label;
-					op_btn_list += '<li class="' + v.key.replace("$", "") + ' active"><a href="javascript:void(0);" onclick="$.selected_menu(\'' + v.key+ '\',\'' + v.label + '\')">' + v.label + '</a></li>';
-				} else {
-					op_btn_list += '<li class="' + v.key.replace("$", "") + '"><a href="javascript:void(0);" onclick="$.selected_menu(\'' + v.key + '\',\'' + v.label + '\')">' + v.label + '</a></li>';
+			if(v.main !== undefined) {
+				if(v.type == "string") {
+					if(v.selected){
+						if(v.main) {
+							selected_label_key = v.key;
+							selected_label_value = v.label;
+						}
+						op_btn_list += '<li class="' + v.key.replace("$", "") + ' active"><a href="javascript:void(0);" onclick="$.selected_menu(\'' + v.key+ '\',\'' + v.label + '\')">' + v.label + '</a></li>';
+					} else {
+						op_btn_list += '<li class="' + v.key.replace("$", "") + '"><a href="javascript:void(0);" onclick="$.selected_menu(\'' + v.key + '\',\'' + v.label + '\')">' + v.label + '</a></li>';
+					}
 				}
 			}
 		}
 	});
-	$(this).append('<div id="autocomplete"><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span id="' + options.id + '_operator" class="' + selected_label_key.replace("$", "") + '">' + selected_label_value + '</span> <span class="caret"></span></button><ul class="dropdown-menu">' + op_btn_list + '</ul></div><div id="scrollable-dropdown-menu"><input type="search" id="' + options.id + '" class="form-control typeahead' + ((options.class) ? " " + options.class : "") + '" placeholder="' + options.placeholder + '" /></div></div>' + radio + '</div>');
+	$(this).append('<div id="autocomplete"><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown"><span id="' + options.id + '_operator" class="' + selected_label_key.replace("$", "") + '">' + selected_label_value + '</span> <span class="caret"></span></button><ul class="dropdown-menu">' + op_btn_list + '</ul></div><div id="scrollable-dropdown-menu"><input type="search" id="' + options.id + '" class="form-control typeahead' + ((options.class) ? " " + options.class : "") + '" placeholder="' + options.placeholder + '" /></div></div>' + checkbox + '</div>');
 	
 	remoteAutocomplete = new Bloodhound({
 		datumTokenizer: Bloodhound.tokenizers.obj.whitespace("value"),
@@ -129,11 +173,22 @@ $.fn.addTraitAutocomplete = function(options, data, callback) {
 		kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PAGING_LIMIT] = 50;
 		kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_PATTERN] = $("#" + options.id).val();
 		kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_OPERATOR] = ["$EQ"];
-		
-		$.ask_to_service(kAPI, function(response) {
-			console.log(response);
-			$("#content .content-title").text('Output for "' + $("#" + options.id).val() + '"');
-			$("#content .content-body").html("<pre>" + JSON.stringify(response, null, "\t") + "</pre>");
+		$.execTraitAutocomplete(kAPI, function(response) {
+			var the_title = "";
+			if(response.paging.affected > 0) {
+				$("#content .content-title").text('Output for "' + $("#" + options.id).val() + '"');
+				$.each(operators, function(ck, cv) {
+					if(cv.key == kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_OPERATOR][0]) {
+						if(cv.main) {
+							the_title = cv.title;
+						} else {
+							the_title += " " + '<i style="color: #666;">' + cv.title + '</i>';
+						}
+					}
+				});
+				
+				$("#content .content-body").addCollapsible({title: the_title.replace("@pattern@", '<span style="color: #dd1144">"' + $("#" + options.id).val() + '"</span>'), content: '<pre>' + JSON.stringify(response, null, "\t") + '</pre>'});
+			}
 		});
 		return false;
 	}).on("change", function() {
@@ -150,10 +205,24 @@ $.fn.addTraitAutocomplete = function(options, data, callback) {
 		kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_PATTERN] = $("#" + options.id).val();
 		kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_OPERATOR] = ["$" + $("#" + options.id + "_operator").attr("class"), ($("#main_search_operator_i").is(":checked") ? '$i' : '"')];
 		
-		$.ask_to_service(kAPI, function(response) {
-			console.log(response);
-			$("#content .content-title").text('Output for "' + $("#" + options.id).val() + '"');
-			$("#content .content-body").html("<pre>" + JSON.stringify(response, null, "\t") + "</pre>");
+		$.execTraitAutocomplete(kAPI, function(response) {
+			var the_title = "";
+			if(response.paging.affected > 0) {
+				$.each(operators, function(ck, cv) {
+					$.each(kAPI["parameters"][kAPI_REQUEST_PARAMETERS][kAPI_PARAM_OPERATOR], function(cck, ccv) {
+						if(ccv == cv.key) {
+							if(cv.main) {
+								the_title = cv.title;
+							} else {
+								the_title += " " + '<i style="color: #666;">' + cv.title + '</i>';
+							}
+						}
+					});
+				});
+				
+				$("#content .content-title").text('Output for "' + $("#" + options.id).val() + '"');
+				$("#content .content-body").addCollapsible({title: the_title.replace("@pattern@", '<span style="color: #dd1144">"' + $("#" + options.id).val() + '"</span>'), content: '<pre>' + JSON.stringify(response, null, "\t") + '</pre>'});
+			}
 		});
 	}).bind("keydown", "alt+left", function(e) {
 		$.left_panel("close", "", function() {
@@ -168,6 +237,27 @@ $.fn.addTraitAutocomplete = function(options, data, callback) {
 	if (typeof callback == "function") {
 		callback.call(this);
 	}
+};
+$.execTraitAutocomplete = function(kAPI, callback) {
+	$.ask_to_service(kAPI, function(response) {
+		if (typeof callback == "function") {
+			callback.call(this, response);
+		}
+	});
+	$("#content #right_btn").html('<span class="ionicons ion-trash-b"></span> Reset all').fadeIn(300, function() {
+		$("#content #right_btn").on("click", function() {
+			apprise("Are you sure?", {title: "Warning", confirm: true}, function(r) {
+				if(r) {
+					$("#content #right_btn").fadeOut(300, function() {
+						console.log("Reset called" + $.makeid());
+						$("#content .content-title").text("Output");
+						$("#content .content-body").html("");
+						$("input.typeahead.tt-input").val("").focus();
+					});
+				}
+			});
+		});
+	});
 };
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
@@ -216,11 +306,11 @@ $.fn.addAutocomplete = function(options, data, callback) {
 
 
 $(document).ready(function() {
-	$.ask_to_service("list-constants", function(system_constants) {
-		$.ask_to_service(system_constants.results["kAPI_OP_LIST_OPERATORS"], function(r) {
-			$.each(r.results, function(rx, rv) {
+	$.ask_to_service("list-constants", function(system_constants) { // <--- Da mettere nel cookie
+		$.ask_to_service(system_constants.results["kAPI_OP_LIST_OPERATORS"], function(oprts) {
+			$.each(oprts.results, function(rx, rv) {
 				if(rv.label !== undefined) {
-					operators.push({"label": rv.label, "key": rv.key, "selected": rv.selected});
+					operators.push({"label": rv.label, "key": rv.key, "selected": rv.selected, "type": rv.type, "main": rv.main, "title": rv.title});
 				} else {
 					if(rx == "label") {
 						operators.push({"label": rv});
@@ -229,11 +319,11 @@ $(document).ready(function() {
 					}
 				}
 			});
-			$("#left_panel > .panel-body:first-child").after('<div class="panel-header"><h1>' + r["title"] + '</h1></div>')
+			$("#left_panel > .panel-body:first-child").after('<div class="panel-header"><h1>' + oprts.results.title + '</h1></div>')
 			$("#left_panel > .panel-body:last-child").addTraitAutocomplete({
 				id: "main_search",
 				class: "",
-				placeholder: r["placeholder"],
+				placeholder:  oprts.results.placeholder,
 				op: operators
 			}, "remote");
 			$.left_panel("open", "", function() {
--- common/tpl/content.tpl
+++ common/tpl/content.tpl
@@ -1,4 +1,11 @@
 <div id="content">
-	<h1 class="content-title">Output</h1>
+	<h1 class="pull-left content-title">Output</h1>
+	<div class="pull-right">
+		<div class="btn-group">
+			<a href="javascript: void(0);" class="btn btn-default-grey" style="display: none;" id="right_btn"></a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<hr />
 	<div class="content-body"></div>
 </div>
