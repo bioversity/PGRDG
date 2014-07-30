09d2a559103dbf7a85f01581877459eb71d1ff3a
gubi <gubi.ale@iod.io>
2014-07-30 11:50:03 +0200

    Fixed some bugs on small devices

--- common/include/get_contents.php
+++ common/include/get_contents.php
@@ -1,8 +1,12 @@
+<?php
+if(strtolower($page) == "advanced_search") {
+	require_once("common/tpl/left_panel.tpl");
+}
+?>
 <section class="container <?php print (strtolower($page) == "search" && !isset($_GET["q"])) ? "backgrounded" : ""; ?>">
 	<?php
 	switch(strtolower($page)) {
 		case "advanced_search":
-			require_once("common/tpl/left_panel.tpl");
 			require_once("common/tpl/advanced_search_content.tpl");
 			break;
 		default:
